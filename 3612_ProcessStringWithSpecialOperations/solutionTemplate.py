b7bebcf127569d974aaa5af0fbc4d3e547fc22fb
Huu Trong Bui <hbu@a7.de>
2026-06-16 14:09:43 +0200

    finished3612 + changed naming-convention

---
+++ 3612_ProcessStringWithSpecialOperations/solutionTemplate.py
@@ -0,0 +1,34 @@
+def meineLoesung(s):
+    output = ""
+
+    for char in s:
+        match char:
+            case "*":
+                output = output[:-1]
+
+            case "#":
+                output += output
+
+            case "%":
+                output = output[::-1]
+
+            case _:
+                output += char
+
+    return output
+
+def optimaleLoesung():
+    """
+    Meine war schon die besste 100% runtime| 98% memory
+    """
+    return
+
+def main():
+    input="" 
+    print("--- meine Lösung ---")
+    print(meineLoesung(input))
+    print("--- optimale Lösung ---")
+    print(optimaleLoesung())
+
+if __name__=="__main__":
+    main()
